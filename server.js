48457c4b3eea23b2a269a0b0db993df52fd09fcd
Shinon2023 <siwat61719@gmail.com>
2024-06-06 12:38:24 +0700

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const PORT = 5000;
 app.use(cors());
 app.use(bodyParser.json());
 app.use("/api", stockRoutes);
+app.use("/api", stockRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
